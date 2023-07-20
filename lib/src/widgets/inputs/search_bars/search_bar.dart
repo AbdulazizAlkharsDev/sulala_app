acf75b27f28667a6da29e709c96ceec8815ecea9
AbdulazizAlkharsDev <139373790+AbdulazizAlkharsDev@users.noreply.github.com>
2023-07-20 13:48:49 +0300

    search bars added

---
+++ lib/src/widgets/inputs/search_bars/search_bar.dart
@@ -0,0 +1,113 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:flutter/material.dart';
+import 'package:sulala_app/src/theme/colors/colors.dart';
+import 'package:sulala_app/src/theme/fonts/fonts.dart';
+
+class PrimarySearchBar extends StatefulWidget {
+  final ValueChanged<String> onChange;
+  final String hintText;
+
+  const PrimarySearchBar({
+    Key? key,
+    required this.onChange,
+    required this.hintText,
+  }) : super(key: key);
+
+  @override
+  _PrimarySearchBarState createState() => _PrimarySearchBarState();
+}
+
+class _PrimarySearchBarState extends State<PrimarySearchBar> {
+  final TextEditingController _textEditingController = TextEditingController();
+  late FocusNode _focusNode;
+  bool isFocused = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode = FocusNode();
+    _focusNode.addListener(_onFocusChange);
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  void _onChanged(String value) {
+    widget.onChange(value);
+    setState(() {});
+  }
+
+  void _onFocusChange() {
+    setState(() {
+      isFocused = _focusNode.hasFocus;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isTyping = _textEditingController.text.isNotEmpty;
+
+    final Color hintTextColor =
+        isTyping ? AppColors.grayscale90 : AppColors.grayscale50;
+
+    return Container(
+      decoration: BoxDecoration(
+        color: AppColors.grayscale0,
+        borderRadius: BorderRadius.circular(24.0),
+        border: Border.all(
+          color: isFocused ? AppColors.primary30 : AppColors.grayscale20,
+          width: 1.0,
+        ),
+      ),
+      child: TextField(
+        controller: _textEditingController,
+        onChanged: _onChanged,
+        focusNode: _focusNode,
+        onTap: () {
+          _focusNode.requestFocus();
+        },
+        onEditingComplete: () {
+          _focusNode.unfocus();
+        },
+        decoration: InputDecoration(
+          hintText: widget.hintText,
+          hintStyle: AppFonts.body2(
+            color: hintTextColor,
+          ),
+          contentPadding: const EdgeInsets.symmetric(horizontal: 16.0),
+          border: InputBorder.none,
+          prefixIcon: const Icon(
+            Icons.search,
+            color: AppColors.grayscale90,
+          ),
+        ),
+        style: AppFonts.body2(
+          color: AppColors.grayscale90,
+        ),
+        textAlignVertical: TextAlignVertical.center,
+      ),
+    );
+  }
+}
+
+
+
+// Example of use:
+
+
+//  SizedBox(
+//               height: 48,
+//               width: 343,
+//               child: PrimarySearchBar(
+//                 onChange: (value) {
+//                   print('Search query: $value');
+//                   print('#################');
+//                 },
+//                 hintText: 'Search by name or ID',
+//               ),
+//             ),
--- lib/test/testing_pages.dart
+++ lib/test/testing_pages.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:sulala_app/src/widgets/controls_and_buttons/icon_buttons/flat_icon_button.dart';
+import 'package:sulala_app/src/widgets/inputs/search_bars/button_search_bar.dart';
 
 class ExamplePage extends StatefulWidget {
   const ExamplePage({Key? key}) : super(key: key);
@@ -24,12 +25,20 @@ class _ExamplePageState extends State<ExamplePage> {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            FlatIconButton(
-              status: FlatIconButtonStatus.loading,
-              icon: Icons.add,
-              onPressed: () {
-                print('Button pressekkd');
-              },
+            SizedBox(
+              height: 48,
+              width: 343,
+              child: ButtonSearchBar(
+                onChange: (value) {
+                  print('Search query: $value');
+                  print('#################');
+                },
+                hintText: 'Search',
+                icon: Icons.filter_alt_outlined,
+                onIconPressed: () {
+                  print('Button pressed');
+                },
+              ),
             ),
             const SizedBox(height: 16),
             FlatIconButton(
