67bc05ef705e7b2f997bbb285444cbf9b427b2ff
AbdulazizAlkharsDev <139373790+AbdulazizAlkharsDev@users.noreply.github.com>
2023-07-24 18:42:27 +0300

    Working on password field

--- lib/test/testing_pages.dart
+++ lib/test/testing_pages.dart
@@ -1,26 +1,17 @@
 // ignore_for_file: library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
-
-import 'package:sulala_app/src/widgets/inputs/dates_fields/disabled_date_field.dart';
-import 'package:sulala_app/src/widgets/inputs/dates_fields/disabled_label_date_field.dart';
-
+import 'package:sulala_app/test/password_field.dart'; // Replace with the actual path to your PasswordField widget
 
 class ExamplePage extends StatefulWidget {
-  const ExamplePage({Key? key}) : super(key: key);
+  const ExamplePage({super.key});
 
   @override
   _ExamplePageState createState() => _ExamplePageState();
 }
 
 class _ExamplePageState extends State<ExamplePage> {
-  final TextEditingController _textEditingController = TextEditingController();
-
-  @override
-  void dispose() {
-    _textEditingController.dispose();
-    super.dispose();
-  }
+  String? _hashedPassword;
 
   @override
   Widget build(BuildContext context) {
@@ -28,28 +19,41 @@ class _ExamplePageState extends State<ExamplePage> {
       appBar: AppBar(
         title: const Text('Example Page'),
       ),
-      body: const Center(
+      body: Center(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             SizedBox(
               width: 300,
-              child: DisabledDateField(
-                hintText: 'Select a date',
+              child: PasswordField(
+                errorText: _hashedPassword == null
+                    ? "Please enter at least 8 charecters"
+                    : null,
+                onPasswordChanged: (password) {
+                  setState(() {
+                    _hashedPassword = password;
+                  });
+                },
               ),
             ),
-            SizedBox(height: 100),
-            SizedBox(
-              width: 300,
-              child: DisabledLabelDateField(
-                labelText: 'Date of Birth',
-                hintText: 'Select a date',
-              ),
-            ),
-            SizedBox(height: 100),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          if (_hashedPassword == null) {
+            print("Please enter the password.");
+          } else {
+            // Here you would perform actual authentication with the server or local logic.
+            // For this example, we'll assume the correct password is 'example123'.
+            if (_hashedPassword == 'aaaaaaaa') {
+              print("Login Successful!");
+            } else {
+              print("Invalid Password!");
+            }
+          }
+        },
+        child: const Icon(Icons.done),
       ),
     );
   }
