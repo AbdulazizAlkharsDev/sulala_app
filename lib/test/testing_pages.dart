7ee6f0761ade2ccf7db325ac47243f91b90fd7ac
AbdulazizAlkharsDev <139373790+AbdulazizAlkharsDev@users.noreply.github.com>
2023-07-24 20:39:53 +0300

    Password Fields Done but need to check again how to control the text in it

--- lib/test/testing_pages.dart
+++ lib/test/testing_pages.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
-import 'package:sulala_app/test/password_field.dart'; // Replace with the actual path to your PasswordField widget
+import 'package:sulala_app/src/widgets/inputs/password_fields/disabled_password_field.dart'; // Replace with the actual path to your PasswordField widget
 
 class ExamplePage extends StatefulWidget {
   const ExamplePage({super.key});
@@ -11,48 +11,28 @@ class ExamplePage extends StatefulWidget {
 }
 
 class _ExamplePageState extends State<ExamplePage> {
-  String? _hashedPassword;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Example Page'),
       ),
-      body: Center(
+      body: const Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             SizedBox(
               width: 300,
-              child: PasswordField(
-                errorText: _hashedPassword == null
-                    ? "Please enter at least 8 charecters"
-                    : null,
-                onPasswordChanged: (password) {
-                  setState(() {
-                    _hashedPassword = password;
-                  });
-                },
+              child: DisabledPasswordField(
+                labelText: 'Password',
+                hintText: 'Enter your password',
               ),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          if (_hashedPassword == null) {
-            print("Please enter the password.");
-          } else {
-            // Here you would perform actual authentication with the server or local logic.
-            // For this example, we'll assume the correct password is 'example123'.
-            if (_hashedPassword == 'aaaaaaaa') {
-              print("Login Successful!");
-            } else {
-              print("Invalid Password!");
-            }
-          }
-        },
+        onPressed: () {},
         child: const Icon(Icons.done),
       ),
     );
