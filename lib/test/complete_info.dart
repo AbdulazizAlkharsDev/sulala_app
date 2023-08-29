1cae0810a3bcb2bcbffc1f5fa49c4b4c72db06eb
AbdulazizAlkharsDev <139373790+AbdulazizAlkharsDev@users.noreply.github.com>
2023-08-29 06:30:55 +0300

    Update complete_info.dart

--- lib/test/complete_info.dart
+++ lib/test/complete_info.dart
@@ -1,6 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'dart:io';
 import 'package:sulala_app/src/theme/colors/colors.dart';
 import 'package:sulala_app/src/theme/fonts/fonts.dart';
 import 'package:sulala_app/src/widgets/controls_and_buttons/buttons/primary_button.dart';
