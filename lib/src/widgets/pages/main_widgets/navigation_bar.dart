594ce11540a4bb6d4f7a442056823570ed2e5502
AbdulazizAlkharsDev <139373790+AbdulazizAlkharsDev@users.noreply.github.com>
2023-09-10 10:07:41 +0300

    Some small changes

--- lib/src/widgets/pages/main_widgets/navigation_bar.dart
+++ lib/src/widgets/pages/main_widgets/navigation_bar.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:sulala_app/src/screens/reg_mode/reg_home_page.dart';
 import '../../../screens/profile/profile_page.dart';
 import '../../../../test/testing_pages.dart';
-import '../../../screens/guest_mode/homescreen_guest_mode.dart';
 import '../../../theme/colors/colors.dart';
 import '../../../theme/fonts/fonts.dart';
 
